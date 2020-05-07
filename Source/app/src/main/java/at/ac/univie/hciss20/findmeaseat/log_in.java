2c9b69ebbfbcd56fd24b022999a782df66d2f8ad
Your Name <you@example.com>
2020-05-07 08:36:26 +0200

    layout: start, log_in, register

--- Source/app/src/main/java/at/ac/univie/hciss20/findmeaseat/log_in.java
+++ Source/app/src/main/java/at/ac/univie/hciss20/findmeaseat/log_in.java
@@ -2,7 +2,9 @@ package at.ac.univie.hciss20.findmeaseat;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class log_in extends AppCompatActivity {
 
@@ -11,4 +13,10 @@ public class log_in extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
     }
+/*
+    public void go_main(View view) {
+        Intent change = new Intent(this, main.class);
+        startActivity(change);
+    }
+*/
 }
